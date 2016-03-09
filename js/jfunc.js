ec40590ae9bc376797587976d1d62358a24ba1dc
djmlee <komikon94@gmail.com>
2016-03-09 18:32:39 -0500

    Initial Site Commit

---
+++ js/jfunc.js
@@ -0,0 +1,28 @@
+$(document).ready( function() {
+
+	$('#aboutme').click( function () {
+		window.location = "/Users/dinsdalelee/Documents/HTML-Project/ParallaxPersonalSite/aboutme.html";
+	}); //About Me
+	
+	$('#cisc3150').click( function () {	
+		alert('cisc3150.html not created yet');
+	}); //CISC3150 Programming Projects
+
+	$('#cisc3110').click( function () {
+		window.location = "http://acc6.its.brooklyn.cuny.edu/~dinlee/";
+	}); //Advanced C++
+	
+	$('#personal').click( function () {
+		alert('personal.html not created yet');
+	}); //Personal
+
+	$('#youtube').click( function () {
+		window.location = "https://www.youtube.com/c/komikon94";
+	}); //Youtube
+
+	$('#home').click( function () {
+		window.location = "/Users/dinsdalelee/Documents/HTML-Project/ParallaxPersonalSite/homepage.html";
+	}); //Home
+	
+	
+});//end
